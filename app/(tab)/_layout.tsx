f6a995d0b030b173bd24cd960387cd3dc4886cd6
vipinrao009 <vipinrao2018@gmail.com>
2025-08-29 17:40:58 +0530

    Refactor tab layout and update navigation structure; remove About and Profile screens, add Calls, Community, and Updates screens.

--- app/(tab)/_layout.tsx
+++ app/(tab)/_layout.tsx
@@ -1,49 +1,161 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { View } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Text, useWindowDimensions, View } from "react-native";
 
 const _layout = () => {
+  const {height} = useWindowDimensions()
   return (
-    <Tabs>
-        <Tabs.Screen
-            name='index' 
-            options={{
-                title:'Home',
-                tabBarIcon: ({color,size}) => <FontAwesome size={28} name='home' color={color}/>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          height: height*0.11,
+          paddingTop: 15,
+          paddingBottom:12
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Chat",
+          tabBarLabel: () => (
+            <Text
+              style={{
+                fontSize: 16,
+                color: "black",
+                fontWeight: "bold",
+                marginTop: 12,
+              }}
+            >
+              Chats
+            </Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={[
+                {
+                  height: 50,
+                  width: 80,
+                  borderRadius: 50,
+                  justifyContent: "center",
+                  alignItems: "center",
+                },
+                focused && { backgroundColor: "#c2efcfff" },
+              ]}
+            >
+              <MaterialIcons
+                name={focused ? "chat" : "chat"}
+                size={34}
+                color={focused ? "green" : "black"}
+              />
+            </View>
+          ),
+        }}
+      />
 
-            }}/>
-        <Tabs.Screen 
-            name='profile' 
-            options={{
-                title:'Profile',
-                tabBarIcon:({color})=> {
-                    return (
-                        <View
-                           style={{
-                            width:60,
-                            height:60,
-                            justifyContent:'center',
-                            alignItems:'center',
-                            backgroundColor:'red',
-                            marginBottom:40,
-                            borderRadius:50
-                           }}
-                        >
-                            <FontAwesome size={28} name='user' color={color}/>
-                        </View>
-                    )
-                }
-                }}/>
-        <Tabs.Screen
-            name="about"
-            options={{
-                title: 'About',
-                tabBarIcon: ({ color }) => <FontAwesome size={28} name='about' color={color} />,
-            }}>
-        </Tabs.Screen>
+      <Tabs.Screen
+        name="updates"
+        options={{
+          title: "Chat",
+          tabBarLabel: () => (
+            <Text
+              style={{
+                fontSize: 16,
+                color: "black",
+                fontWeight: "bold",
+                marginTop: 12,
+              }}
+            >
+              Updates
+            </Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={[
+                {
+                  height: 50,
+                  width: 80,
+                  borderRadius: 50,
+                  justifyContent: "center",
+                  alignItems: "center",
+                },
+                focused && { backgroundColor: "#c2efcfff" },
+              ]}
+            >
+              <MaterialIcons
+                name="update"
+                size={34}
+                color={focused ? "green" : "black"}
+              />
+            </View>
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="community"
+        options={{
+          tabBarLabel: () => (
+            <Text style={{ fontSize: 16, fontWeight: "bold", marginTop: 10 }}>
+              Community
+            </Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={[
+                {
+                  height: 50,
+                  width: 80,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 50,
+                },
+                focused && { backgroundColor: "#c2efcfff" },
+              ]}
+            >
+              <Ionicons
+                name={focused ? "people" : "people-outline"}
+                size={34}
+                color={focused ? "green" : "black"}
+              />
+            </View>
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="calls"
+        options={{
+          title: "Calls",
+          tabBarLabel:()=>(
+            <Text style={{fontWeight:'bold', marginTop:10, fontSize:16}}>Calls</Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={[
+                {
+                  height: 50,
+                  width: 80,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 50,
+                },
+                focused && { backgroundColor: "#c2efcfff" },
+              ]}
+            >
+              <Ionicons
+                name={focused ? "call" : "call-outline"}
+                size={34}
+                color={focused ? "green" : "black"}
+              />
+            </View>
+          ),
+        }}
+      />
     </Tabs>
-  )
-}
+  );
+};
 
-export default _layout
+export default _layout;
---
+++ app/(tab)/calls.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const calls = () => {
+  return (
+    <View>
+      <Text>calls</Text>
+    </View>
+  )
+}
+
+export default calls
